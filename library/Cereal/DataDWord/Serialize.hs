9a7b47d9a3a6f3cf9470dfc71494e50650390021
Nikita Volkov <nikita.y.volkov@mail.ru>
2018-08-11 23:22:45 +0300

    Add more instances, implementing them via Generic

--- library/Cereal/DataDWord/Serialize.hs
+++ library/Cereal/DataDWord/Serialize.hs
@@ -5,14 +5,11 @@ module Cereal.DataDWord.Serialize
 where
 
 import Cereal.DataDWord.Prelude
-import qualified Cereal.DataDWord.Put as Put
-import qualified Cereal.DataDWord.Get as Get
 
 
-instance Serialize Word96 where
-  put = Put.word96 put put
-  get = Get.word96 get get
-
-instance Serialize Word128 where
-  put = Put.word128 put
-  get = Get.word128 get
+instance Serialize Word96
+instance Serialize Word128
+instance Serialize Word160
+instance Serialize Word192
+instance Serialize Word224
+instance Serialize Word256
